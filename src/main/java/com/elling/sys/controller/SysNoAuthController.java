00dd3744bd3ba20010104e19240bd6d1a98a7eeb
cyygith <770764225@qq.com>
2020-01-17 18:02:00 +0800

    register controller

---
+++ src/main/java/com/elling/sys/controller/SysNoAuthController.java
@@ -0,0 +1,49 @@
+package com.elling.sys.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elling.common.entity.Result;
+import com.elling.common.utils.DateUtil;
+import com.elling.sys.model.SysUser;
+import com.elling.sys.service.SysUserService;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value="/sysNoAuth")
+public class SysNoAuthController {
+	private Logger logger = Logger.getLogger(SysNoAuthController.class);
+	
+	@Autowired
+    SysUserService sysUserService;
+	
+	
+	/**
+	 * .注册
+	 * @param sysUser
+	 * @return
+	 */
+	@RequestMapping("registerUser")
+    public Result add(@RequestBody SysUser sysUser) {
+    	try {
+    		SysUser user = sysUserService.findBy("username", sysUser.getUsername());
+    		if(user!=null) {
+    			return Result.error("该手机号已经已经存在，请使用其他手机号注册");
+    		}else {
+    			sysUser.setCreateTime(DateUtil.getNowTime());
+    			sysUserService.saveModelAndDept(sysUser);
+    		}
+	    }catch(Exception e) {
+    		e.printStackTrace();
+    		logger.error(e.getMessage());
+    		return Result.error(e.getMessage());
+    	}
+	    return Result.success();
+    }
+	
+	
+}
