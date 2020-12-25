3ad11e750eae0377efa9298df8e8caa3b312d442
cyygith <770764225@qq.com>
2020-12-25 22:20:42 +0800

    20201225 cyy add a DateTimeFormate

---
+++ src/main/java/com/elling/common/utils/DateTimeFormate.java
@@ -0,0 +1,73 @@
+package com.elling.common.utils;
+
+/**
+ * .时间格式常量
+ * @author Administrator
+ *
+ */
+public class DateTimeFormate {
+	/**
+	 * .年月
+	 */
+	public static String FORMATE_MONTH = "yyyy-MM";
+	/**
+	 * .年月日
+	 */
+	public static String FORMATE_DATE = "yyyy-MM-dd";
+	/**
+	 * .时分秒
+	 */
+	public static String FORMATE_HMD = "HH:mm:ss";
+	/**
+	 * .年月日
+	 */
+	public static String FORMATE_DATE_1 = "yyyyMMdd";
+	/**
+	 * .年月日 时
+	 */
+	public static String FORMATE_HOUR = "yyyy-MM-dd HH";
+	/**
+	 * .年月日 时分
+	 */
+	public static String FORMATE_MINUTE = "yyyy-MM-dd HH:mm";
+	
+	/**
+	 * .年月日 时分秒
+	 */
+	public static String FORMATE_TIME = "yyyy-MM-dd HH:mm:ss";
+	/**
+	 * .年月日 时分秒
+	 */
+	public static String FORMATE_TIME_STR = "yyyyMMddHHmmss";
+	
+	
+	/**
+	 * .年
+	 */
+	public static String AREA_YEAR = "yyyy";
+	/**
+	 * .月
+	 */
+	public static String AREA_MONTH = "MM";
+	
+	/**
+	 * .日
+	 */
+	public static String AREA_DAY = "dd";
+	
+	/**
+	 * .小时
+	 */
+	public static String AREA_HOUR = "HH";
+	
+	/**
+	 * .分钟
+	 */
+	public static String AREA_MINUTE = "mm";
+	
+	/**
+	 * .秒
+	 */
+	public static String AREA_SECOND = "ss";
+	
+}
